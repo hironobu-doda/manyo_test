b38ba26379db9418b9a568d6b60029e56670d148
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-03-06 17:39:17 +0900

    task.spec.rbで「タスク一覧画面に遷移したら、作成済みのタスクが表示される」ことをテストした

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,16 @@
 require_relative 'boot'
 
+# config.generators do |g|
+#   g.test_framework :rspec,
+#                    fixtures: true,
+#                    view_specs: false,
+#                    helper_specs: false,
+#                    routing_specs: false,
+#                    controller_specs: false,
+#                    request_specs: false
+#   g.fixture_replacement :factory_bot, dir: "spec/factories"
+# end
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
