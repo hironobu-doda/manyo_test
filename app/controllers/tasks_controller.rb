b5e473a4ab2af347f93eb186db426ac205d7a5c2
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-03-06 11:22:52 +0900

    編集画面を作成した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    Task.create(task_params)
-    redirect_to new_task_path
+    @task = Task.new(task_params)
+    if @task.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to tasks_path, notice: "新しく作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render 'new'
+    end
   end
 
   def index
@@ -15,6 +21,19 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "ブログを編集しました！"
+    else
+      render 'edit'
+    end
   end
 
   private
