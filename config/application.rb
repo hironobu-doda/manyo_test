5655277424b8e1a277f9456542f9a7a95905e9ab
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-03-08 10:34:38 +0900

    タイムゾーンの設定を日本にした

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module ManyoTest
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
