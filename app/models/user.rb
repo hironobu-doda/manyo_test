b19f3addc42578991ebf04db06996e5ca9123bf3
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-03-15 12:29:59 +0900

    ユーザのアドレスにユニーク制約を貼った

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :name, presence: true,length: {maximum: 30}
   validates :email, precence: true,length: (maximum: 255),
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation {email.downcase!}
 end
