295716415ba88a2d011520e6e61c09212f94f0ae
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-03-18 15:36:48 +0900

    ログインしている時は、ユーザー登録画面（new画面）に行かせないように、コントローラで制御した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required
   def new
+    redirect_to user_path(current_user.id) if current_user
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if  @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  # get 'sessions/new'
+  # get 'tasks/index'
 
   root 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
 
   resources :users, only: [:new, :create,:show]
+
+  resources :sessions, only: [:new, :create, :destroy]
 end
