fccb49b6c68d9a1eaf38a9f8050b149c4e9253d2
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-03-19 20:37:12 +0900

    管理者画面を作成した（一覧、新規作成、詳細、編集、削除画面を作成した）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -8,4 +8,6 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password_digest, presence: true, length: { minimum: 6 }
+
+
 end
