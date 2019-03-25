8f450868088355c52e16274075460a9cc24140e7
hironobu_doda <ironnamono.rakusaku@gmail.com>
2019-03-25 16:59:57 +0900

    動作確認

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery/dist/jquery.js
 //= require activestorage
 //= require turbolinks
 //= require bootstrap/dist/js/bootstrap.min
